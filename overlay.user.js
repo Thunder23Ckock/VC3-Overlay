1ee71b88e83db926eef19151ef5659470a66b64a
Thunder23Ckock <140283389+Thunder23Ckock@users.noreply.github.com>
2023-07-23 00:28:16 +0200

    Create overlay.user.js
    VC3 overlay

---
+++ overlay.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         r/sprachkanal3 Template
+// @namespace    http://tampermonkey.net/
+// @version      8.2
+// @description  try to take over the canvas!
+// @author       placeDE-revolution Devs
+// @match        https://garlic-bread.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// ==/UserScript==
+
+var overlayImage = null;
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+        const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+        overlayImage = document.createElement("img");
+        updateImage();
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;pointerEvents: 'none';`;
+        canvasContainer.appendChild(overlayImage);
+    }, false);
+}
+
+function updateImage() {
+    overlayImage.src = "https://cdn.discordapp.com/attachments/1132408210314252408/1132435840220528671/overlay_target2.0.png?" + Date.now()
+}
+
+setInterval(function () {overlayImage.src = "https://cdn.discordapp.com/attachments/1132408210314252408/1132435840220528671/overlay_target2.0.png?" + Date.now()}, 30000);
